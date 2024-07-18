96fe8b6c3b82387091740e1dc59602a11cc6d696
mostafkhalaf11 <mostafkhalaf11@gmail.com>
2024-07-18 16:30:16 +0300

    changing android license internet commit

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:weather/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather/main.dart';
 import 'package:weather/models/weather_model.dart';
 
